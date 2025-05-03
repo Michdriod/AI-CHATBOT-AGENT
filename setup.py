b6a82220e346ab0100380ce805e754fd211f0003
Michdriod <Oluwasegunmichealalejo@gmail.com>
2025-05-03 14:21:07 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Customer AI-Assistant',
+    packages=find_packages(),
+    version='0.0.0',
+    author='Alejo Micheal',
+    author_email= 'oluwasegunmichealaljo@gamil.com',
+    install_requires=[]
+)
